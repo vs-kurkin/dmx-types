65614b2a8114a61248be33af8a2ec6954d9553b0
VK <mail@b-vladi.ru>
2024-05-16 10:27:49 +0300

    Initial

---
+++ types/main.d.ts
@@ -0,0 +1,43 @@
+// noinspection JSUnusedGlobalSymbols
+
+export declare interface Serial {
+  path: string;
+  manufacturer: string;
+  serialNumber: string;
+  pnpId: string;
+  friendlyName: string;
+  vendorId: string;
+  productId: string;
+}
+
+export interface UniverseOptions {
+  host?: string;
+  port?: string;
+  baudRate?: number;
+  dataBits?: number;
+  stopBits?: number;
+  universe?: number;
+  interval?: number;
+  id: string;
+  path: string;
+  driver: string;
+}
+
+export interface Device {
+  channels: Channel[];
+  mode?: Map<string, Channel[]>;
+  universe?: string;
+  vendor?: string;
+  model?: string;
+}
+
+export interface Channel {
+  type: string;
+  value: number;
+  name?: string;
+  default?: number;
+  min?: number;
+  max?: number;
+  amount?: number;
+  steps?: number[];
+}

1ea1087e487791a8556407304da1a30dc657d117
VK <mail@b-vladi.ru>
2024-06-04 18:47:49 +0300

    Release 0.0.14

--- types/device.d.ts
+++ types/device.d.ts
@@ -2,18 +2,19 @@
 export declare interface Device {
   channels: Channel[];
   mode?: Record<string, Channel[]>;
+  model?: string;
+  name?: string;
   universe?: string;
   vendor?: string;
-  model?: string;
 }
 
 export declare interface Channel {
+  amount?: number;
+  default?: number;
+  max?: number;
+  min?: number;
+  name?: string;
+  steps?: number[];
   type: string;
   value: number;
-  name?: string;
-  default?: number;
-  min?: number;
-  max?: number;
-  amount?: number;
-  steps?: number[];
 }
